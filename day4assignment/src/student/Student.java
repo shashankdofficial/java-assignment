a2f46c2804d1446212aae774acdaef8c3ca3a4eb
shashankrd14 <allyouget14@gmail.com>
2022-05-20 00:30:39 +0530

    Update

---
+++ day4assignment/src/student/Student.java
@@ -0,0 +1,18 @@
+package student;
+
+public class Student {
+
+	void StudentDetail(int roll,int age,int marks,String name) {
+		  if(age<18 || age>60)
+		  {
+			 System.out.println("Enter valid Age"); 
+		  }
+		  if(marks<0 || marks>500)
+		  {
+			  System.out.println("Enter valid MArks");
+		  }
+		  
+		  System.out.println("The Roll is:" + roll +"\n"+"The age is:" + age +"\n"+"The Marks are:" +  marks +"\n"+ "The name is:"+ name);
+	  }
+
+}
--- day4assignment/src/student/StudentDetails.java
+++ day4assignment/src/student/StudentDetails.java
@@ -2,30 +2,20 @@ package student;
 
 public class StudentDetails {
 
-	int roll;
-	String name;
-	int age;
-	//zero argument constructor
-	public StudentDetails() {
-		roll= 01;
-		name= "Shashank";
-		age = 24;
-	}
-	//overloaded parameterized constructor
-	public StudentDetails(int roll, String name, int age) {
-		this.roll = roll;
-		this.name = name;
-		this.age = age;
-	}
-	public void showDetails() {
-		System.out.println("Roll Number : "+roll);
-		System.out.println("Employee Name : "+name);
-		System.out.println("Age is : "+age);
-	}
-	public static void main(String[] args) {
-		StudentDetails std1=new StudentDetails();
-		std1.showDetails();
-		StudentDetails std2=new StudentDetails(02, "Gautam", 25);
-		std2.showDetails();
-	}
+	Student s1 = new Student();
+	   
+	   StudentDetails(){
+		   
+	   }
+	   StudentDetails(int roll,int age,int marks,String name){
+		   s1.StudentDetail(roll,age,marks,name);
+	   }
+	   
+	   public static void main(String[] args) {
+		
+		   StudentDetails student1 = new StudentDetails();
+		   student1.s1.StudentDetail(1, 24, 600, "Shashank");
+		   
+		   StudentDetails student2 = new StudentDetails(2, 25, 450, "Gutam");
+	   }
 }

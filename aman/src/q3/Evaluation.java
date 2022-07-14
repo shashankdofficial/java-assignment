d2d87a5bad3458e5dc65c4980e558d50415e355d
shashankrd14 <allyouget14@gmail.com>
2022-07-14 22:46:13 +0530

    Update

---
+++ aman/src/q3/Evaluation.java
@@ -0,0 +1,13 @@
+package q3;
+
+public abstract class Evaluation {
+
+	private final int numberOfQuestions = 4;
+	abstract void evaluationTiming();
+	
+	void printNoOfQuestions() {
+	System.out.println("No. of Questions in Evaluation is :" +numberOfQuestions);
+	}
+
+	
+}
